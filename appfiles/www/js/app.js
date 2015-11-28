b42047589fbfa5bde81f0384a2d5a16e8f85b159
andykat <ahtz@mit.edu>
2015-11-27 22:19:59 -0800

    Added name to self profile page

--- appfiles/www/js/app.js
+++ appfiles/www/js/app.js
@@ -116,6 +116,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           }
       }
   })
+  .state('sidemenu.tab.selfprofile', {
+      url: '/selfprofile',
+      views: {
+          'selfprofile': {
+              templateUrl: 'templates/selfprofile.html',
+              controller: 'SelfProfileCtrl'
+          }
+      }
+  })
    
 
   // if none of the above states are matched, use this as the fallback
