da64b13a3ab10192d12907fe6b403266bf708eed
Robert Knop <robert.knop@nordakademie.org>
2015-11-27 18:42:27 -0800

    Refactoring routes

--- serverfiles/mainServer.js
+++ serverfiles/mainServer.js
@@ -1,103 +1,12 @@
 'use strict';
 var express = require('express');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+var routes = require('./routes');
 var app = express();
-
-// create application/json parser
-var jsonParser = bodyParser.json();
+routes(app);
 
 app.get('/', function (req, res) {
     res.send('Hello World! Podcast are the best thing on earth!');
-});
-
-app.get('/api/v1/users/:id', function (req, res) {
-    MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
-        if (err) {
-            throw err;
-        }
-        db.collection("users").findOne({"_id": req.params.id}, function (err, requestedUser) {
-            if (err) {
-                throw err;
-            }
-            if (requestedUser) {
-                console.log("GET user: ");
-                console.log(requestedUser);
-                res.send(requestedUser);
-            } else {
-                res.send({message: "No user found!"});
-            }
-            db.close();
-        });
-    });
-});
-
-app.get('/api/v1/users/search/:query', function (req, res) {
-    MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
-        if (err) {
-            throw err;
-        }
-        db.collection("users").find({
-            $or: [
-                {_id: req.params.query},
-                {firstName: req.params.query},
-                {lastName: req.params.query}
-            ]
-        }).limit(20).toArray(function (err, requestedUsers) {
-            if (err) {
-                throw err;
-            }
-            if (requestedUsers.length <= 0) {
-                res.send({message: "No user found!"});
-            } else {
-                console.log("Search result: ");
-                console.log(requestedUsers);
-                res.send(requestedUsers);
-            }
-            db.close();
-        });
-    });
-});
-
-app.get('/api/v1/users', function (req, res) {
-    MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
-        if (err) {
-            throw err;
-        }
-        db.collection("users").find().limit(100).toArray(function (err, allUsers) {
-            if (err) {
-                throw err;
-            }
-            console.log("GET all users: ");
-            console.log(allUsers);
-            res.send(allUsers);
-            db.close();
-        });
-    });
-});
-
-app.post('/api/v1/signup', jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    console.log("POST user: ");
-    req.body.createdOn = new Date();
-    console.log(req.body);      // your JSON
-    MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
-        if (err) {
-            throw err;
-        }
-        db.collection("users").insert(req.body, function (err) {
-            if (err) {
-                console.log("ERROR: " + err.message);
-                res.send({"message": "ERROR: User already exists!"});
-                //throw err;
-            } else {
-                console.log("POST user: ");
-                console.log(req.body);
-                res.send({"message": "User added"});    // echo the result back
-                db.close();
-            }
-        });
-    });
 });
 
 var server = app.listen(8080, function () {
