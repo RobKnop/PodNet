73d29e10c2cf4408c9d62a2189ae72d9ec5ba425
andykat <ahtz@mit.edu>
2015-11-29 14:07:18 -0800

    search now links to view other person's profile

--- appfiles/www/js/app.js
+++ appfiles/www/js/app.js
@@ -153,7 +153,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           }
       }
   })
-
+  .state('sidemenu.tab.otherprofile', {
+      url: '/otherprofile/:_id',
+      views: {
+          'otherprofile': {
+              templateUrl: 'templates/otherprofile.html',
+              controller: 'OtherProfileCtrl'
+          }
+      }
+  })
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
 
--- appfiles/www/js/controllers.js
+++ appfiles/www/js/controllers.js
@@ -9,6 +9,7 @@
 })*/
 angular.module('starter.controllers', [])
 .controller('LoginCtrl', function ($scope, $state, $http, $ionicPopup) {
+    //remove windowlocalstorage so can't use last login
     $scope.signIn = function (user) {
         
 
@@ -153,6 +154,7 @@ angular.module('starter.controllers', [])
                 }
                 else {
                     $scope.searchResults = resp.data;
+                    window.localStorage['searchResults'] = JSON.stringify(resp.data);
                 }
 
             }, function (err) {
@@ -169,7 +171,35 @@ angular.module('starter.controllers', [])
     }
 
 })
+.controller('OtherProfileCtrl', function ($scope, $stateParams, $state) {
+    var searchResults = JSON.parse(window.localStorage['searchResults']);
+    var index = -1;
+    for (var i = 0; i < searchResults.length; i++) {
+        if (searchResults[i]._id == $stateParams._id) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) {
+        console.log("Error did not find search id");
+        $state.go('sidemenu.tab.search');
+    }
+
+    window.localStorage['viewData'] = JSON.stringify(searchResults[index]);
+    $scope.name = searchResults[index].firstName + " " + searchResults[index].lastName;
+    $scope.nfollowers = searchResults[index].followers.length;
+
+
+    if (searchResults[index].following.length) {
+        $scope.nfollowing = searchResults[index].following.length;
+    }
+    else {
+        $scope.nfollowing = 0;
+    }
+})
+
 .controller('SelfProfileCtrl', function ($scope) {
+    window.localStorage['viewData'] = window.localStorage['selfData'];
     var selfData = JSON.parse(window.localStorage['selfData']);
     //console.log('selfData', selfData);
     $scope.name = selfData.firstName + " " + selfData.lastName;
@@ -186,15 +216,15 @@ angular.module('starter.controllers', [])
     
 })
 .controller('TabPostsCtrl', function ($scope) {
-    var selfData = JSON.parse(window.localStorage['selfData']);
+    var selfData = JSON.parse(window.localStorage['viewData']);
 })
 .controller('TabFollowingCtrl', function ($scope) {
-    var selfData = JSON.parse(window.localStorage['selfData']);
+    var selfData = JSON.parse(window.localStorage['viewData']);
     $scope.following = selfData.following;
     console.log("following", $scope.following);
 })
 .controller('TabFollowersCtrl', function ($scope) {
-    var selfData = JSON.parse(window.localStorage['selfData']);
+    var selfData = JSON.parse(window.localStorage['viewData']);
     $scope.followers = selfData.followers;
     console.log("followers", selfData.followers);
 })
